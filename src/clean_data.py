a8be9cdab60e533f86158d1755c006342fc63b29
Madan-KU <ku.madan05@gmail.com>
2023-08-22 16:46:36 +0100

    updated

--- src/clean_data.py
+++ src/clean_data.py
@@ -3,57 +3,74 @@ import logging
 # import argparse
 import pandas as pd
 import yaml
+from sklearn.impute import SimpleImputer
+
 
 # Read the YAML configuration
 def read_yaml_config(file_path):
     with open(file_path, 'r') as file:
         return yaml.safe_load(file)
-    
+
+
 config = read_yaml_config('parameters.yaml')
 
+# Configuring logging using YAML values
 logging.basicConfig(level=config['logging']['level'],
                     format=config['logging']['format'],
-                    handlers=[logging.FileHandler(config['logging']['log_file']), logging.StreamHandler()])
+                    handlers=[logging.FileHandler(config['logging']['log_file']),
+                               logging.StreamHandler()])
 
 def read_data():
     raw_file_path=config['data']['raw']
 
     if not os.path.exists(raw_file_path):
         logging.warning(f"Directory {raw_file_path} does not exist.")
+
         return None
     
     for file in os.listdir(raw_file_path):
 
-        print(f'1{raw_file_path}')
-        
-        if file.endswith('.csv'):
+        if file.endswith(".csv"):
             try:
                 raw_file_path=os.path.join(raw_file_path,file)
                 df=pd.read_csv(raw_file_path)
-                return df
+                logging.info(f"Successfully read {file}")
+                return df,file
             
             except Exception as e:
                 logging.error(f"Error reading {file}: {e}")
+                continue
 
-        logging.warning(f"No CSV file found in {raw_file_path}.")
-        return None
-        
-
-        
+    logging.warning(f"No CSV file found in {raw_file_path}.")
+    return None
 
 
+def clean_data(df):
+    logging.info("Cleaning data...")
 
+    target=config['base']['target_col']
+    
+    df.dropna(subset=[target], inplace=True)
+    imputer = SimpleImputer(strategy='mean')
+    cleaned_df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
 
+    logging.info("Imputed missing values using SimpleImputer")
 
-
-
-
-
+    return cleaned_df
 
 def main():
-    df=read_data()
-    # print(config['data']['raw'])
+    df,filename=read_data()
+    filename='cleaned_'+ filename
+    output_path = os.path.join(config['data']['cleansed'], filename)
 
+    if df is not None:
+        cleaned_data=clean_data(df)
+        cleaned_data.to_csv(output_path, index=False)
+        logging.info(f"'{filename}' loaded to '{config['data']['cleansed']}'")
+
+    else:
+        logging.warning("No valid data was read.") 
+    
 
 if __name__ == "__main__":
     main()
--- src/get_data.py
+++ src/get_data.py
@@ -23,7 +23,7 @@ logging.basicConfig(level=config['logging']['level'],
                     format=config['logging']['format'],
                     handlers=[logging.FileHandler(config['logging']['log_file']), logging.StreamHandler()])
 
-csvfile= ''
+filename= ''
 
 def read_data_from_directory(directory_path):
 
@@ -31,29 +31,29 @@ def read_data_from_directory(directory_path):
         logging.warning(f"Directory {directory_path} does not exist.")
         return None
 
-    for csvfile in os.listdir(directory_path):
-        if csvfile.endswith(".csv"):
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".csv"):
             try:
-                df = pd.read_csv(os.path.join(directory_path, csvfile))
-                logging.info(f"Successfully read {csvfile}")
-                return df,csvfile
+                df = pd.read_csv(os.path.join(directory_path, filename))
+                logging.info(f"Successfully read {filename}")
+                return df,filename
             except Exception as e:
-                logging.error(f"Error reading {csvfile}: {e}")
+                logging.error(f"Error reading {filename}: {e}")
     
     logging.warning(f"No CSV file found in {directory_path}.")
-    print(csvfile)
     return None
 
 def main():
-    df,csvfile = read_data_from_directory(config['data']['remote'])
+    df,filename = read_data_from_directory(config['data']['remote'])
+    filename='raw_'+ filename
 
     if df is not None:
         print(df)
         
-    output_path = os.path.join(config['data']['raw'], csvfile)
-    df.to_csv(output_path)
+    output_path = os.path.join(config['data']['raw'], filename)
+    df.to_csv(output_path, index=False)
 
-    logging.info(f"'{csvfile}' loaded to '{config['data']['raw']}'")
+    logging.info(f"'{filename}' loaded to '{config['data']['raw']}'")
 
 if __name__ == "__main__":
     main()
